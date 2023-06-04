bce5617f2f368a17156538ec6b4b2d3f04aa1a30
azimuthality <azizov.asimkpc322020@mail.ru>
2023-06-04 13:54:00 +0300

    Initial commit

---
+++ src/main/java/com/example/eadditivesbot/EAdditivesBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.eadditivesbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAdditivesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAdditivesBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eadditivesbot/EAdditivesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eadditivesbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAdditivesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
